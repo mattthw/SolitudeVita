0129c3747041cf5a62e6e8a6eed9cfb7d2dbbe31
Matt <imattxc@gmail.com>
2020-05-09 03:35:01 -0700

    add new Citadel map remake :)
    menu fix

--- source/menu.c
+++ source/menu.c
@@ -2583,7 +2583,8 @@ typedef struct
 
 level_t		levels[] =
 {
-	{"narsp", "Narrows"},
+	{"citadel", "Citadel"},
+	{"narsp", "Narrows V2"},
 	{"narrowed", "Narrows (original)"},
 	{"base", "Minibase"},
 	{"plaza", "Plaza"},
@@ -2653,7 +2654,8 @@ typedef struct
 
 episode_t	episodes[] =
 {
-	{"Solitude Mappack", 0, 8} //only 1 episode (solitude maps)
+	{"New Maps", 0, 2},
+	{"Classic Maps", 2, 7}
 };
 
 //MED 01/06/97  added hipnotic episodes
@@ -2851,7 +2853,7 @@ void M_GameOptions_Draw (void)
 	else
 		M_Print (160, 96, va("%i minutes", (int)timelimit.value));
 
-	M_Print (0, 112, "                "); //"Episode" 
+	M_Print (0, 112, "        Map Pack"); //"Episode" 
    //MED 01/06/97 added hipnotic episodes
    if (hipnotic)
       M_Print (160, 112, hipnoticepisodes[startepisode].description);
@@ -2964,7 +2966,7 @@ void M_NetStart_Change (int dir)
 
 	case 7:
 		startepisode += dir;
-		count = 1;
+		count = 2; //episodes list size
 
 		if (startepisode < 0)
 			startepisode = count - 1;
