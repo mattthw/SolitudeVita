3de0ac20a2b34149045626dd1033ba0dc45c9ddb
Matt <imattxc@gmail.com>
2020-05-17 11:37:54 -0700

    Menu GUI changes
    - add GUI window frame for menu's without one
    - create helper method for drawing windows
    - clean up pause menu
    - adjust quit menu

--- source/draw.h
+++ source/draw.h
@@ -25,6 +25,8 @@ extern	qpic_t		*draw_disc;	// also used on sbar
 
 void Draw_Init (void);
 void Draw_Character (int x, int y, int num);
+void Draw_Window(float x, float y, float width, float height, char *str);
+void Draw_CenterWindow(float width, float height, char *str);
 void Draw_DebugChar (signed char num);
 void Draw_Pic (int x, int y, qpic_t *pic);
 void Draw_AlphaPic (int x, int y, qpic_t *pic, float alpha);
